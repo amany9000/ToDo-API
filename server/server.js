09e9492c7d730d9f3153d8a8f2b77119bf4556e4
amany9000 <201651007@iiitvadodara.ac.in>
2018-04-14 00:18:59 +0530

    added post method through express

--- server/server.js
+++ server/server.js
@@ -1,3 +1,23 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+
 var mongoose = require("./db/mongoose.js")
 var todo = require("./models/todo.js").todo
 var user = require("./models/user.js").user
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post("/todos", (req,res) => {
+	var Todo = new todo({
+		text : req.body.text
+	});
+	Todo.save().then((doc) => {
+		res.send(doc);
+	}, (e) => {
+		res.status(400).send(e);
+	})
+})
+app.listen(3000, ()=> {
+	console.log("Started on port 3000");
+})  
