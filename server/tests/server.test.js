e37f54dc702a54bed5d23be8086457cd7c2c70ee
amany9000 <201651007@iiitvadodara.ac.in>
2018-06-21 18:14:32 +0530

    Created tests for get-todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,15 @@ var request = require("supertest")
 var app = require("./../server.js").app
 var todo = require("./../models/todo.js").todo
 
+const testTodos = [{
+	text : 'The destruction of of the ONE Ring'
+},{
+	text : 'Picking up some ice cream after that'	
+}] 
 beforeEach((done) => {
-	todo.remove({}).then(() => done());
+	todo.remove({}).then(() => {
+		return todo.insertMany(testTodos); 
+	}).then(()=>done());
 });
 
 describe("Post /todos", () => {
@@ -21,8 +28,8 @@ describe("Post /todos", () => {
 			})
 			.end((err,res) => {
 				if(err)
-					return done(err)
-				todo.find().then((result) =>{
+					return done({err})
+				todo.find({text}).then((result) =>{
 					expect(result.length).toBe(1);
 					expect(result[0].text).toBe(text);
 					done();
@@ -39,9 +46,20 @@ describe("Post /todos", () => {
 				if(err)
 					return done(err)
 				todo.find().then((result) =>{
-					expect(result.length).toBe(0);
+					expect(result.length).toBe(2);
 					done();
 				}).catch((e) => done(e));
 			}) 
 	});
+})
+describe('GET /todos', () => {
+	it('Should get all todos', (done) => {
+		request(app)
+			.get("/todos")
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
+	})
 })
