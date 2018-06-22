647f222a7346d9b92b98f00decaf4e128b66ec10
amany9000 <201651007@iiitvadodara.ac.in>
2018-06-22 18:46:58 +0530

    Included Get TodosId

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express")
 var bodyParser = require("body-parser")
+const {ObjectID} = require("mongodb")
 
 var mongoose = require("./db/mongoose.js")
 var todo = require("./models/todo.js").todo
@@ -26,6 +27,21 @@ app.get("/todos", (req,res) => {
 		res.status(400).send(e);
 	});
 })
+
+app.get("/todos/:id",(req,res) => {
+	var id = req.params.id
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	todo.findById(id).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.status(200).send({todo});
+	}).catch((e) => {
+		res.status(400).send();});
+});
 
 app.post("/users", (req,res) => {
 	var User = new user({
