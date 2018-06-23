4dfae4050b2ce38c2f96d914e09de3c5f5f733e7
amany9000 <201651007@iiitvadodara.ac.in>
2018-06-23 17:09:15 +0530

    Setting Up Delete Request for Heroku

--- server/server.js
+++ server/server.js
@@ -44,6 +44,21 @@ app.get("/todos/:id",(req,res) => {
 		res.status(400).send();});
 });
 
+app.delete("/todos/:id",(req,res) => {
+	var id = req.params.id
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.status(200).send({todo});
+	}).catch((e) => {
+		res.status(400).send();});
+});
+
 app.post("/users", (req,res) => {
 	var User = new user({
 		email : req.body.email
